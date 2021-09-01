3ec347672a9632c8a7810885c1d9d7aa5afa8fc5
Liang Ding <liangding86@gmail.com>
2021-09-01 09:33:22 -0400

    Update ibool_reader.py

--- DSEM_Utils/ibool_reader.py
+++ DSEM_Utils/ibool_reader.py
@@ -32,7 +32,8 @@ def read_ibool_by_scipy(ibool_file, NSPEC_global):
 
     return ibool
 
-
+#############################################
+# read all selected GLL points in one element.
 def extract_element_125(ibool_file, NSPEC_global, index_element):
     '''
     * Extract the global index of all 125 GLL points in the selected element.
@@ -108,3 +109,86 @@ def DEnquire_Element(ibool_file, NSPEC_global, index_element, nGLL_per_element=-
         return extract_element_27(ibool_file, NSPEC_global, index_element)
     else:
         return extract_element_125(ibool_file, NSPEC_global, index_element)
+
+    
+#############################################
+# read all selected GLL points in one slice. 
+def extract_ibool_125(ibool_file, NSPEC_global):
+    '''
+    * Extract the indexes of the selected GLL points.
+    * All 125 GLL points in each element are selected.
+
+    :param file_path:       The file path of the ibool file. Data type: STR.
+    :param NSPEC_global:    The number of element in total. Data type: int.
+    :return:
+    (1) The global name of the selected GLL points.
+    (2) The array containing the element index and local GLL index.
+        Data: [[i_spec, i_gll], [i_spec, i_gll], ..., [i_spec, i_gll]]
+    '''
+
+    ibool = read_ibool_by_scipy(ibool_file, NSPEC_global)
+    n_spec, n_gll = np.shape(ibool)
+    names_GLL_arr = []
+    index_GLL_arr = []
+    current_gll = -1
+    for i_spec in range(n_spec):
+        for i_gll in range(n_gll):
+            if ibool[i_spec][i_gll] > current_gll:
+                current_gll = ibool[i_spec][i_gll]
+                names_GLL_arr.append(current_gll)
+                index_GLL_arr.append(np.array([i_spec, i_gll]))
+
+    return np.hstack(names_GLL_arr), np.vstack(index_GLL_arr)
+
+
+
+def extract_ibool_27(ibool_file, NSPEC_global):
+    '''
+    * Extract the indexes of the selected GLL points.
+    * Only 27 GLL points are selected per element.
+
+    :param file_path:       The file path of the ibool file. STR.
+    :param NSPEC_global:    The number of element in total. INT
+    :return:
+    (1) The global name of the selected GLL points.
+    (2) The array containing the element index and local GLL index.
+        Data: [[i_spec, i_gll], [i_spec, i_gll], ..., [i_spec, i_gll]]
+    '''
+
+    ibool = read_ibool_by_scipy(ibool_file, NSPEC_global)
+    n_spec, n_gll = np.shape(ibool)
+    names_GLL_arr = []
+    index_GLL_arr = []
+    current_gll = -1
+    INDEX27 = np.asarray(CONSTANT_INDEX_27_GLL).astype(int)
+
+    # extract the global unique index of selected GLL points.
+    for i_spec in range(n_spec):
+        for i_gll in INDEX27:
+            if ibool[i_spec][i_gll] > current_gll:
+                current_gll = ibool[i_spec][i_gll]
+                names_GLL_arr.append(current_gll)
+                index_GLL_arr.append(np.array([i_spec, i_gll]))
+
+    return np.hstack(names_GLL_arr), np.vstack(index_GLL_arr)
+
+
+
+def DEquire_ibool(ibool_file, NSPEC_global, nGLL_per_element=-1):
+    '''
+    * Enquire all GLL information from *.ibool file.
+
+    :param ibool_file:          The *.ibool file.
+    :param NSPEC_global:        The number of element in the *.ibool file..
+    :param nGLL_per_element:    The number of GLL points tended to enquire in each element.
+    :return:
+    (1) The global name of the selected GLL points.
+    (2) The array containing the element index and local GLL index.
+        Data: [[i_spec, i_gll], [i_spec, i_gll], ..., [i_spec, i_gll]]
+    '''
+
+    # extract the selected 27 GLL points in each element.
+    if 27 == nGLL_per_element:
+        return extract_ibool_27(ibool_file, NSPEC_global)
+    else:
+        return extract_ibool_125(ibool_file, NSPEC_global)
